59b057c4e32c17c612cc9e742ee8973eecfd33ed
Oluwatimilehin Abiodun <oluwatimilehinabiodun64@gmail.com>
2025-12-20 21:31:26 +0100

    Modified: Remove custom serializer MyTokenObtainPairSerializer

--- todo_list/serializers.py
+++ todo_list/serializers.py
@@ -29,10 +29,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "tasks"]
-
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        token['name'] = user.name
-        return token
