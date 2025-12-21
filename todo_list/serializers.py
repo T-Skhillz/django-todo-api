095b1da54bb7aa6dc7b28570a6a6d9bfc8e345bb
Oluwatimilehin Abiodun <oluwatimilehinabiodun64@gmail.com>
2025-12-21 16:47:41 +0100

    Add user registration endpoint

--- todo_list/serializers.py
+++ todo_list/serializers.py
@@ -29,3 +29,30 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "tasks"]
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "password2"]
+        extra_kwargs = {
+            "email": {"required": True}
+        }
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError({
+                "password": "Passwords do not match!"
+            })
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("password2")
+        user = User.objects.create_user(
+            username = validated_data["username"],
+            email = validated_data["email"],
+            password = validated_data["password"]
+        )
+        return user
