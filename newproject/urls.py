1207ef679a6cb8baaf593712ab5072617d320633
Oluwatimilehin Abiodun <oluwatimilehinabiodun64@gmail.com>
2025-12-22 15:43:10 +0100

    Remove health check endpoint

--- newproject/urls.py
+++ newproject/urls.py
@@ -27,7 +27,7 @@ def home(request):
     })
 
 urlpatterns = [
-    path("", home),
+    #path("", home),
     path('admin/', admin.site.urls),
     path('api/auth/login/', TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
